a7d92a4f896816ef4de79c6d8aa50e7a09c68ab0
Fredy Geovany Martir González <fredymartir@icasa.com.gt>
2024-03-20 20:38:21 -0600

    Avance 2

--- app/src/main/java/com/umg/abe_umg/MainActivity.java
+++ app/src/main/java/com/umg/abe_umg/MainActivity.java
@@ -1,6 +1,10 @@
 package com.umg.abe_umg;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView lblResultado;
+    EditText txtIngreso;
+    Button btnvalidar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        txtIngreso = (EditText) findViewById(R.id.txtIngreso);
+        lblResultado = (TextView) findViewById(R.id.lblResultado);
+        btnvalidar = (Button) findViewById(R.id.btnValidar);
+
+        btnvalidar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Cadena = txtIngreso.getText().toString();
+                ArbolBinario ABE = new ArbolBinario(Cadena);
+
+                lblResultado.setText(""+ABE.EvaluaExpresion());
+            }
+        });
+
     }
 }
