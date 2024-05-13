93409d4235b4d1a1e9543342973c3bf6d7231f1d
FREDY GEOVANY MARTIR GONZALEZ <fredymartir@icasa.com.gt>
2024-05-13 14:47:42 -0600

    Version estable sobre el dibujado del arbol y ordenamientos.
    Falta dise;o y acomodamiento de los componentes tnto vertical como horizontal.

--- app/src/main/java/com/umg/abe_umg/Arbol.java
+++ app/src/main/java/com/umg/abe_umg/Arbol.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,82 +16,107 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Arbol extends AppCompatActivity {
+public class Arbol extends AppCompatActivity
+{
+    //Variables Globales
     private ArbolBinario Arbol;
     ArbolBinario ABE;
     private String datos;
-    public static final int DIAMETRO = 200;
-    public static final int RADIO = DIAMETRO / 2;
-    public static final int ANCHO = 230;
     public void setArbol(ArbolBinario arbol)
     {
+        //Con los valores traidos del activity padre se setea el arbol binario
         this.Arbol = arbol;
-
-        //repaint();
     }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arbol);
+        //Se validan los datos otorgados por el activity padre los cuales se valida si son null o si traen datos
         Bundle parametros = this.getIntent().getExtras();
         if (parametros!=null)
         {
             datos = parametros.getString("cadena");
         }
         ConstraintLayout layout1 = findViewById(R.id.main);
-        Lienzo1 fondo = new Lienzo1(this);
+        Lienzo1 fondo = new Lienzo1(this); //Se gener a un objeto de tipo lienzo
         layout1.addView(fondo);
     }
-    class Lienzo1 extends View {
-
-        public Lienzo1(Context context) {
-            super(context);
-        }
-
-        protected void onDraw(Canvas canvas) {
-
-                super.onDraw(canvas);
-                int ancho = canvas.getWidth();
-                int alto = canvas.getHeight();
-                Paint paint = new Paint();
-                ABE = new ArbolBinario(datos);
-                setArbol(ABE);
-//        paint.setStrokeWidth(10);
-//        paint.setARGB(255,255,0,0);
-//        canvas.drawLine(100,100,600,800,paint);
-//        paint.setARGB(255,255,128,0);
-//        canvas.drawCircle(600,600,500,paint);
-           pintar(canvas,30,60,Arbol.Raiz);
-        }
-        public void pintar(Canvas g, int x, int y, NodoArbol subArbol)
+    class Lienzo1 extends View
+    {
+        private final Paint mPaint;
+        private Paint nodoPaint;
+        private Paint linePaint;
+        private int nodoRadio = 50; // Tamaño del nodo
+        public Lienzo1(Context context)
         {
-            Paint.FontMetrics fm = new Paint.FontMetrics();
-            Paint mTxtPaint = new Paint();
-            Paint paint = new Paint();
-            if (subArbol!=null)
+            super(context);
+            mPaint = new Paint();
+            mPaint.setColor(Color.WHITE );
+            mPaint.setStrokeWidth(5);
+            init();
+        }
+        private void init()
+        {
+            nodoPaint = new Paint();
+            nodoPaint.setTextSize(40); // Ajusta el tamaño del texto
+            nodoPaint.setStyle(Paint.Style.FILL);
+            nodoPaint.setColor(Color.rgb( 39, 237, 231 )); // Cambia el color del texto a blanco
+            //Otros atributos de inicialización
+            linePaint = new Paint();
+            linePaint.setColor(Color.rgb( 176, 58, 243 ));
+            linePaint.setStrokeWidth(3);
+        }
+        protected void onDraw(Canvas canvas)
+        {
+            super.onDraw(canvas);
+            ABE = new ArbolBinario(datos);
+            setArbol(ABE);
+            //drawTree(canvas,getWidth() / 2,100,Arbol.Raiz);
+            drawTree(canvas, Arbol.Raiz, getWidth() / 2, 50, getWidth() / 4, 0);
+        }
+        private void drawTree(Canvas canvas, NodoArbol nodo, int x, int y, int xOffset, int depth)
+        {
+            //La variable xOffset es la compensacion en el eje X
+            //La variable depth es la profundidad de los niveles en el arbol
+            if (nodo == null)
+                return; //Si el arbol es null genera un return y no sigue el codigo
+
+            // Dibuja el nodo actual
+            canvas.drawCircle(x, y, nodoRadio, nodoPaint);
+            String text = String.valueOf(nodo.Dato);
+            Rect bounds = new Rect(); //Se crea un objeto de tipo Rect que puede calcular las dimensiones de un rectangulo
+
+            nodoPaint.setColor(Color.RED); // Cambia el color del texto a blanco
+            nodoPaint.getTextBounds(text, 0, text.length(), bounds);//Con el objeto bounds obtenemos las dimensiones del texto para acomodarlo en el dibujado del circulo
+            canvas.drawText(text, x - bounds.exactCenterX(), y - bounds.exactCenterY(), nodoPaint);
+            init();
+
+            // Dibuja las líneas a los hijos y luego dibuja los hijos
+            if (nodo.Izquierdo != null)
             {
-                int EXTRA = Arbol.NodosCompletos(subArbol)*ANCHO/2;
-                paint.setARGB(255,0,0,0);
-                mTxtPaint.setColor(Color.RED);
-                mTxtPaint.setTextSize(50);
-                mTxtPaint.getFontMetrics(fm);
-                //g.drawOval(x+800, y-400, DIAMETRO, ANCHO,paint);
-                g.drawText(subArbol.Dato.toString(), x+400+fm.top, y+800+fm.bottom,mTxtPaint);
-
-
-                //https://azzits.wordpress.com/tag/android-drawtext-with-background-color/
-
-                if (subArbol.Izquierdo != null)
-                {
-                    g.drawLine(x+350, y +800, x+650 - ANCHO - EXTRA, y+700 + ANCHO,paint);
-                }
-                if (subArbol.Derecho != null)
-                {
-                    g.drawLine(x+355 , y +800, x+55  + ANCHO + EXTRA, y+700 + ANCHO,paint);
-                }
-                pintar(g, x+400 - ANCHO - EXTRA, y-80 + ANCHO, subArbol.Izquierdo);
-                pintar(g, x-400 + ANCHO + EXTRA, y-80 + ANCHO, subArbol.Derecho);
+                int childX = x - xOffset; //Se calcula la cordenada para dibujar el nodo hijo con el valor de x - la compesacion
+                int childY = y + 2 * nodoRadio; // La línea debe comenzar desde la parte inferior del nodo padre
+                drawLine(canvas, x, y + nodoRadio, childX, childY);
+                drawTree(canvas, nodo.Izquierdo, childX, childY, xOffset / 2, depth + 1);
+                //Codigo Anterior
+                //drawLine(canvas, x - xOffset, y + nodeRadius, x, y + 2 * nodeRadius);
+                //drawTree(canvas, node.Izquierdo, x - xOffset, y + 2 * nodeRadius, xOffset / 2, depth + 1);
             }
+            if (nodo.Derecho != null)
+            {
+                int childX = x + xOffset;
+                int childY = y + 2 * nodoRadio; // La línea debe comenzar desde la parte inferior del nodo padre
+                drawLine(canvas, x, y + nodoRadio, childX, childY);
+                drawTree(canvas, nodo.Derecho, childX, childY, xOffset / 2, depth + 1);
+                //Codigo Anterior
+                //drawLine(canvas, x + xOffset, y + nodeRadius, x, y + 2 * nodeRadius);
+                //drawTree(canvas, node.Derecho, x + xOffset, y + 2 * nodeRadius, xOffset / 2, depth + 1);
+            }
+        }
+        private void drawLine(Canvas canvas, int startX, int startY, int stopX, int stopY)
+        {
+            canvas.drawLine(startX, startY, stopX, stopY, linePaint);
         }
     }
 }
--- app/src/main/java/com/umg/abe_umg/MainActivity.java
+++ app/src/main/java/com/umg/abe_umg/MainActivity.java
@@ -2,9 +2,7 @@ package com.umg.abe_umg;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Canvas;
 import android.os.Bundle;
-import android.text.style.UpdateLayout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,50 +11,55 @@ import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText et1;
+public class MainActivity extends AppCompatActivity
+{
     ArbolBinario ABE = new ArbolBinario();
-    TextView lblResultado,txtOrden;
+    TextView lblResultado,txtPreOrden,txtInOrden,txtPosOrden;
     EditText txtIngreso;
     Button btnvalidar,btnDibujar;
 
-    class Vista extends View {
-
+    class Vista extends View
+    {
         public Vista(Context context) {
             super(context);
         }
-
     }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-
         txtIngreso = (EditText) findViewById(R.id.txtIngreso);
         lblResultado = (TextView) findViewById(R.id.lblResultado);
-        txtOrden = (TextView) findViewById(R.id.txtOrden);
+        txtPreOrden = (TextView) findViewById(R.id.txtPreOrden);
+        txtInOrden = (TextView) findViewById(R.id.txtInOrden);
+        txtPosOrden = (TextView) findViewById(R.id.txtPosOrden);
         btnvalidar = (Button) findViewById(R.id.btnValidar);
         btnDibujar = (Button) findViewById(R.id.btnDibujar);
 
-        btnvalidar.setOnClickListener(new View.OnClickListener() {
-
+        btnvalidar.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                Canvas canvas = new Canvas();
+            public void onClick(View v)
+            {
                 String Cadena = txtIngreso.getText().toString();
                 ABE = new ArbolBinario(Cadena);
                 lblResultado.setText(""+ABE.EvaluaExpresion());
 
-                txtOrden.setText(ABE.toString(1));
+                //Llenado de los TextBox en el xml con los ordenes del arbol
+                txtPreOrden.setText(ABE.toString(0));
+                txtInOrden.setText(ABE.toString(1));
+                txtPosOrden.setText(ABE.toString(2));
             }
         });
 
-        btnDibujar.setOnClickListener(new View.OnClickListener() {
+        btnDibujar.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 String Cadena = txtIngreso.getText().toString();
 
                 //Traslado de informacion al segundo Activity
