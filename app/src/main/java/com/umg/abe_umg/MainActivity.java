6b1fec6509112f37408892d285b38d48dfdd4123
FREDY GEOVANY MARTIR GONZALEZ <fredymartir@icasa.com.gt>
2024-05-16 12:49:50 -0600

    App v1.53 estable, no incorpora aun la validacion de numeros negativos.
    Teclado virtual, se captura el enter y se oculta automaticamente despues de dar enter y ejecuta el codigo de Evaluar.

--- app/src/main/java/com/umg/abe_umg/MainActivity.java
+++ app/src/main/java/com/umg/abe_umg/MainActivity.java
@@ -3,10 +3,14 @@ package com.umg.abe_umg;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,6 +31,65 @@ public class MainActivity extends AppCompatActivity
             super(context);
         }
     }
+    public void onf()
+    {
+        String Cadena = txtIngreso.getText().toString();
+        ABE = new ArbolBinario(Cadena);
+
+        // Verificar si la entrada contiene caracteres no permitidos
+        boolean entradaValida = true;
+        for (char c : Cadena.toCharArray())
+        {
+            if (!Character.isDigit(c) && !esOperadorMatematico(c) && !esParentesis(c))
+            {
+                entradaValida = false;
+                break;
+            }
+        }
+
+        if (entradaValida)
+        {
+            // Se valida la cadena si comienza con alguno de los operadores
+            Pattern pattern = Pattern.compile("^[\\+\\-\\*\\/^].*");
+            Matcher matcher = pattern.matcher(Cadena);
+
+            // Se verifica si cumple o no
+            if (matcher.find())
+            {
+                lblResultado.setText("Aun no se admiten negativos");
+            }
+            else
+            {
+                // Validar si la expresion contiene el caracter no divicible
+                if (Cadena.contains("/0"))
+                {
+                    lblResultado.setText("No se puede dividir ente 0");
+                }
+                else
+                {
+                    lblResultado.setText("" + ABE.EvaluaExpresion());
+                }
+            }
+        }
+        else
+        {
+            lblResultado.setText("Expresion sin poder calcular");
+        }
+        //Llenado de los TextBox en el xml con los ordenes del arbol
+        txtPreOrden.setText(String.format("PREORDEN: %s", ABE.toString(0)));
+        txtInOrden.setText(String.format("INORDEN: %s", ABE.toString(1)));
+        txtPosOrden.setText(String.format("POSTORDEN: %s", ABE.toString(2)));
+    }
+    // Metodo para verificar si un caracter es un operador matematico
+    private boolean esOperadorMatematico(char c)
+    {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+    }
+    // Metodo para verificar si un caracter es un parentesis
+    private boolean esParentesis(char c)
+    {
+        return c == '(' || c == ')';
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -42,66 +105,27 @@ public class MainActivity extends AppCompatActivity
         btnvalidar = (Button) findViewById(R.id.btnValidar);
         btnDibujar = (Button) findViewById(R.id.btnDibujar);
 
+        EditText editText = findViewById(R.id.txtIngreso);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    // Aquí ejecutas tu procedimiento
+                    onf();
+                    //Para ocultar el teclado Virtual
+                    imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
         btnvalidar.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View v)
-            {
-                String Cadena = txtIngreso.getText().toString();
-                ABE = new ArbolBinario(Cadena);
-                // Verificar si la entrada contiene caracteres no permitidos
-                boolean entradaValida = true;
-                for (char c : Cadena.toCharArray())
-                {
-                    if (!Character.isDigit(c) && !esOperadorMatematico(c) && !esParentesis(c))
-                    {
-                        entradaValida = false;
-                        break;
-                    }
-                }
-
-                if (entradaValida)
-                {
-                    // Se valida la cadena si comienza con alguno de los operadores
-                    Pattern pattern = Pattern.compile("^[\\+\\-\\*\\/^].*");
-                    Matcher matcher = pattern.matcher(Cadena);
-
-                    // Se verifica si cumple o no
-                    if (matcher.find())
-                    {
-                        lblResultado.setText("No se puede iniciar con un operador");
-                    }
-                    else
-                    {
-                        // Validar si la expresion contiene el caracter no divicible
-                        if (Cadena.contains("/0"))
-                        {
-                            lblResultado.setText("No se puede dividir ente 0");
-                        }
-                        else
-                        {
-                            lblResultado.setText("" + ABE.EvaluaExpresion());
-                        }
-                    }
-                }
-                else
-                {
-                    lblResultado.setText("Expresion Invalida");
-                }
-                //Llenado de los TextBox en el xml con los ordenes del arbol
-                txtPreOrden.setText(String.format("PREORDEN: %s", ABE.toString(0)));
-                txtInOrden.setText(String.format("INORDEN: %s", ABE.toString(1)));
-                txtPosOrden.setText(String.format("POSTORDEN: %s", ABE.toString(2)));
-            }
-            // Metodo para verificar si un caracter es un operador matematico
-            private boolean esOperadorMatematico(char c)
-            {
-                return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
-            }
-            // Metodo para verificar si un caracter es un parentesis
-            private boolean esParentesis(char c)
-            {
-                return c == '(' || c == ')';
+            public void onClick(View v) {
+                onf();
             }
         });
 
